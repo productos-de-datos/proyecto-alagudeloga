f6784d6998ca576e72bd46f3c22c57ac1291b9e9
Alejandro Agudelo Galeano <80123225+alagudeloga@users.noreply.github.com>
2022-06-28 21:56:22 -0500

    Modelo

--- src/features/make_features.py
+++ src/features/make_features.py
@@ -1,3 +1,7 @@
+"""
+Se lee el archivo creado en el python compute_daily_prices.py y se escribe nuevamente en la ruta indicada.
+"""
+
 def make_features():
     """Prepara datos para pronóstico.
 
@@ -12,10 +16,16 @@ def make_features():
     analizar y determinar las variables explicativas del modelo.
 
     """
-    raise NotImplementedError("Implementar esta función")
+    import pandas as pd
+
+    df_caract = pd.read_csv(r'data_lake/business/precios-diarios.csv',index_col=None,header=0)
+    df_caract["fecha"] = pd.to_datetime(df_caract["fecha"]).dt.strftime('%Y-%m-%d')
+    df_caract.to_csv("data_lake/business/features/precios-diarios.csv",index=None, header=True)
+    return
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
-
+    make_features()
     doctest.testmod()
